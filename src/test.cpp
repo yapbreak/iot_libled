45a8110a2937d009ffa334c81349445514df63fb
Adrien Oliva <olivaa+gitlab@yapbreak.fr>
2018-06-27 09:23:47 +0200

    Fix Cppcheck issue

--- src/test.cpp
+++ src/test.cpp
@@ -10,12 +10,14 @@
 
 // Create a simple led connected to pin 4
 led_t my_led(4);
+led_rgb_t my_led_rgb(1, 2, 3, true);
 
 /**
 * @brief Arduino function to setup and initialize everything.
 */
 void setup() {
     my_led.on();
+    my_led_rgb.set_color(0xff6600);
 }
 
 /**
@@ -23,6 +25,7 @@ void setup() {
 */
 void loop() {
     my_led.toggle();
+    my_led_rgb.toggle();
     delay(1000);
 }
 
